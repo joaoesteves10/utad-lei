eb5b3afc3d2b7846121e0d9d1f37f18efd92dead
nuno muletas <79775803+MuletasNuno@users.noreply.github.com>
2022-10-19 21:48:51 +0100

    Create ex13Muletas.cpp

---
+++ utad - lab/ex13Muletas.cpp
@@ -0,0 +1,35 @@
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    printf (" escolha uma operaÃ§Ã£o das que estÃ£o aqui referidas(+, -, *, /) \n ");  
+    scanf ("%c", &sinal)
+
+    if (&sinal == "+")
+    {
+        printf("Voce selecionou a adiÃ§Ã£o")
+    }
+
+    if (&sinal == "-")
+    {
+        printf("Voce selecionou a subtraÃ§Ã£o")
+    }
+
+    if (&sinal == "*")
+    {
+        printf("Voce selecionou a multiplicaÃ§Ã£o")
+    }
+
+    if (&sinal == "/")
+    {
+        printf("Voce selecionou a divisÃ£o")
+    }
+
+    else
+    {
+        printf("Voce selecionou uma operaÃ§Ã£o invalida")
+        
+    }
+    
+    
+    
+}
