d37a1f6ad7c06873eadb38c5d60a2a3e3077929e
João Esteves <joaoesteves10A4@sapo.pt>
2022-11-03 12:15:41 +0000

    adiciona exercício 20 de lab e 4 de uma random ficha de outra uni

---
+++ extra/m4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <locale.h>
+
+void triangulo_vert(int x) {
+
+  for (int i = 1; i <= x; i++) {
+    for (int n = 1; n <= i; n++) {
+      printf("#");
+    }
+    printf("\n");
+  }
+
+  for (int i = x-1; i > 0; i--) {
+    for (int n = i; n > 0; n--) {
+      printf("#");
+    }
+    printf("\n");
+  }
+}
+
+
+void triangulo_horiz(int x) {
+
+  for (int i = 1; i <= x; i++) {
+
+    for (int n = ((x-i)); n > 0 ; n--){
+      printf(" ");
+    }
+
+    for (int n = 1; n <= 2*i-1; n++) {
+      printf("#");
+    }
+
+    printf("\n");
+  }
+}
+
+int main() {
+
+  int arg;
+  printf("insira o argumento™: ");
+  scanf_s("%d", &arg);
+
+  triangulo_vert(arg);
+  triangulo_horiz(arg);
+
+  return 0;
+}
---
+++ utad - lab/ex20.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+/* 20. Escreva um programa que calcule e apresente no ecrã o valor da função
+1,52x2+5 para todos os valores de x pertencentes ao intervalo [a, b] e com
+incrementos de dx. Utilize uma função no seu programa que receba como parâmetros
+os valores de a, b e dx.
+
+Para ficar mais claro, imagine que querem postar o gráfica da função 1,52x2+5
+entre 1 e 50. O programa teria como entrada a=1, b=50 e dx=1. Isso significa que
+o programa deve imprimir o valor para x=1, x=2, x=3,...,x=50. */
+
+void fun(int a, int b, int dx) {
+  float valor;
+  while (a <= b) {
+    valor = 1.52*a*a+5;
+    printf("%d: %f\n", a, valor);
+    a+=dx;
+  }
+}
+
+
+int main() {
+  setlocale(LC_ALL, "Portuguese");
+
+  fun(1, 50, 1);
+  return 0;
+}
