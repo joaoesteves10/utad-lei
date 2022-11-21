82eeace1c116afc181ea6e7fe80ec755395ec547
João Esteves <joaoesteves10A4@sapo.pt>
2022-11-21 12:05:03 +0000

    oops não estava atento

--- primeiro-ano/primeiro-semestre/procedimental/ex51_1.c
+++ primeiro-ano/primeiro-semestre/procedimental/ex51_1.c
@@ -6,6 +6,7 @@ b. MostrarVetor.
 O programa deve usar alocação dinâmica de memória (o user deve introduzir o número de elementos do vetor, N)*/
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void LerVetor(int *vetor, int N){
   for (int i=0; i<N; i++) {
@@ -22,14 +23,24 @@ void MostrarVetor(int *vetor, int N) {
 
 int main() {
 
-  int N;
+  int N, *V;
   printf("quantos elementos? ");
   scanf_s("%d", &N);
 
-  int V[N];
+  /* aloca o espaço necessário para um vetor de N elementos
+   * o malloc mantém o lixo que estava na memória, o calloc enche a memória com zeros */
+  V = (int *)malloc(N * sizeof(int));
+
+  if (V == NULL) { // isto acontece se não houver memória suficiente
+    printf("erro de alocação");
+    exit(0);
+  }
 
   LerVetor(V, N);
   MostrarVetor(V, N);
+
+  /* liberta memória alocada dinamicamente */
+  free(V);
 
   return 0;
 }
